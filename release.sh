394fcf5049fba81b27a4139ca234f1ce54ab7407
Ray <lens1989@gmail.com>
2016-09-23 00:41:33 +0800

    chore(dist): Initial dist subtree commit

--- release.sh
+++ release.sh
@@ -7,4 +7,8 @@ npm run build
 echo 'adding to git'
 git add dist && git commit -m "chore(dist): Initial dist subtree commit"
 
-git subtree push --prefix dist origin master 
+echo 'split to gh-pages branch'
+git subtree split --prefix dist -b gh-pages
+
+echo 'push to master...'
+git push -f origin gh-pages:master
