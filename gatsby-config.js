d11ca75792391bf72b0c8c4003da3968cdb8d320
liaoyl <liaoyl@my.mail>
2019-03-05 17:24:59 +0800

    feat: gatsby with typescript

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  plugins: [
+    {
+      resolve: `gatsby-plugin-typescript`,
+      options: {
+        isTSX: true, // defaults to false
+        jsxPragma: `jsx`, // defaults to "React"
+        allExtensions: true, // defaults to false
+      },
+    },
+  ],
+}
