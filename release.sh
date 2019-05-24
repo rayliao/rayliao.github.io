c5fd86cfb48170246a0c52695a83a92ed930a09e
rayliao <littlefish@littlefishdeiMac.local>
2019-05-24 10:21:34 +0800

    refactor

--- release.sh
+++ release.sh
@@ -7,8 +7,8 @@ yarn build
 echo 'adding to git'
 git add public -f && git commit -m "chore(public): Initial public subtree commit"
 
-echo 'split to gh-pages branch'
-git subtree split --prefix public -b gh-pages
+echo 'split to gh-release branch'
+git subtree split --prefix public -b gh-release
 
 echo 'push to master...'
-git push -f origin gh-pages:master
+git push -f origin gh-release:master
