96b1c57ee756a331929afaf3b67b0d41947bd7a9
rayliao <littlefish@littlefishdeiMac.local>
2019-05-31 11:18:23 +0800

    feat: test #4

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,19 @@
 module.exports = {
   plugins: [
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `RayLiao`,
+        short_name: `rayliao`,
+        start_url: `/`,
+        background_color: `#f7f0eb`,
+        theme_color: `#a2466c`,
+        display: `standalone`,
+      },
+    },
+    `gatsby-plugin-offline`,
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-glslify`,
     `gatsby-plugin-typescript`,
-    // `gatsby-transformer-typescript-css-modules`,
   ],
 }
