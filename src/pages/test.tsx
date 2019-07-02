e3bde18f65cdc7f75a29755e9486b16b1c85a460
rayliao <littlefish@littlefishdeiMac.local>
2019-07-02 17:46:35 +0800

    feat: 很容易配置 close #2

---
+++ src/pages/test.tsx
@@ -0,0 +1,29 @@
+import { graphql } from 'gatsby'
+import * as React from 'react'
+
+const Test = ({ data }) => {
+  console.log(data)
+  return <div>test</div>
+}
+
+export default Test
+
+export const query = graphql`
+  query ReposQuery {
+    github {
+      viewer {
+        repositories(
+          privacy: PUBLIC
+          affiliations: OWNER
+          isFork: false
+          first: 100
+        ) {
+          nodes {
+            name
+            url
+          }
+        }
+      }
+    }
+  }
+`
