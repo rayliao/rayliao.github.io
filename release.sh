a5f6a7e6a1d935e8c92f695dae48d0254e40922c
rayliao <littlefish@littlefishdeiMac.local>
2019-09-11 10:14:08 +0800

    chore: fix deploy

--- release.sh
+++ release.sh
@@ -10,6 +10,9 @@ yarn build
 echo 'adding to git'
 git add public -f && git commit -m "chore(public): Initial public subtree commit"
 
+echo 'del branch before split'
+git branch -D gh-release
+
 echo 'split to gh-release branch'
 git subtree split --prefix public -b gh-release
 
