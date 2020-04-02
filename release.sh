9acd98d959bb88330438f3e62d8941fd9657f468
rayliao <littlefish@littlefishdeiMac.local>
2020-04-02 14:28:42 +0800

    chore: build #25

--- release.sh
+++ release.sh
@@ -1,20 +1,17 @@
 # release 
 # build production files
 
-echo 'cleaning...'
-gatsby clean
-
 echo 'building ...'
 yarn build 
 
 echo 'adding to git'
-git add public -f && git commit -m "chore(public): Initial public subtree commit"
+git add build -f && git commit -m "chore(build): Initial build subtree commit"
 
 echo 'del branch before split'
 git branch -D gh-release
 
 echo 'split to gh-release branch'
-git subtree split --prefix public -b gh-release
+git subtree split --prefix build -b gh-release
 
 echo 'push to master...'
 git push -f origin gh-release:master
