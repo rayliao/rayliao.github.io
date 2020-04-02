06b9edc2eb028e1d6bac50a561beaa9fbaeeaa22
rayliao <littlefish@littlefishdeiMac.local>
2020-04-02 14:20:36 +0800

    chore(build): Initial build subtree commit

---
+++ build/precache-manifest.feec5fc3407f4d200f152d61395714f1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ab675c27eb43fd670285da3afdf0fc9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51b881f2f091e452ebda",
+    "url": "/static/css/main.733f559a.chunk.css"
+  },
+  {
+    "revision": "5b4b4ab9cd7019edaa99",
+    "url": "/static/js/2.2f817866.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.2f817866.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "51b881f2f091e452ebda",
+    "url": "/static/js/main.e307f032.chunk.js"
+  },
+  {
+    "revision": "eb1d36012e2caf448aad",
+    "url": "/static/js/runtime-main.3b677b86.js"
+  },
+  {
+    "revision": "6c284ac2bcce3bd47a4aa2bff3fa6b91",
+    "url": "/static/media/2018.6c284ac2.jpg"
+  },
+  {
+    "revision": "f500b72c339a3f25921b8f858a1b415d",
+    "url": "/static/media/apple.f500b72c.png"
+  },
+  {
+    "revision": "5affc753d0f126d27272a721bda3f92a",
+    "url": "/static/media/bg.5affc753.jpg"
+  },
+  {
+    "revision": "5e8916e63ea383d012763760e3170407",
+    "url": "/static/media/dada.5e8916e6.gif"
+  }
+]);
