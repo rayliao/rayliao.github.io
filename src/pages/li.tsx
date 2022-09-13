c36234f4b3e0b2eeb7f7fddfdb9fed6b91dc50ca
hooray <lyl@ik.me>
2022-09-13 16:49:30 +0800

    feat: Li and Liao

--- src/pages/li.tsx
+++ src/pages/li.tsx
@@ -1,7 +1,17 @@
 import { NextPage } from "next";
+import Head from "next/head";
+import Family from "components/Family";
 
 const Li: NextPage = () => {
-  return <div>Li</div>;
+  const shuffleList = [1, 2, 5, 6, 7, 8, 10, 11, 12, 13, 14].reverse();
+  return (
+    <>
+      <Head>
+        <title>李 - RayLiao</title>
+      </Head>
+      <Family data={shuffleList} path="li" />
+    </>
+  );
 };
 
 export default Li;
